a4234d61626eb7c1fedd6499ac66905b3622cddf
Faizan Ali <devfaizi435@gmail.com>
2022-09-08 04:17:36 -0700

    axios

--- src/components/Products/useFetchProducts.js
+++ src/components/Products/useFetchProducts.js
@@ -3,13 +3,23 @@ import axios from 'axios';
 
 let useFetchProducts = () =>{
     let [products,setProducts]=useState([]);
+    let [loading,setLoading]=useState();
+    // async function fetchProducts(){
+    //     let productRes=await fetch('https://fakestoreapi.com/products');
+    //     let products=await productRes.json();
+    //     setProducts(products);
+    //     console.log("products",await productRes.json());
+
+    // }
+
     async function fetchProducts(){
-        let productRes=await fetch('https://fakestoreapi.com/products');
-        let products=await productRes.json();
-        setProducts(products);
-        console.log("products",await productRes.json());
+        
+        let products=await axios.get('https://fakestoreapi.com/products');
+        setProducts(products.data);
+        
 
     }
+
     useEffect(()=>{
         fetchProducts();
 
